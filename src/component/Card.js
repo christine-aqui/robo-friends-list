daa6319590385989cebda13ee7e7324ca9967758
christine-aqui <aqui.christine@gmail.com>
2018-11-23 16:19:06 -0500

    checkpoint

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const Card = () => {
+const Card = (robot) => {
   return (
-    <div>
-      <img src='https://robohash.org/test?' alt='robots'/>
+    <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+    <h1>Robo Friends - Hello</h1>
+      <img src='https://robohash.org/test?200x200' alt='robots'/>
       <div>
-        <h2>Name</h2>
-        <p>email.name@mgial.com</p>
+        <h2>{robot.name}</h2>
+        <p>{robot.email}</p>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+import { robots } from './robots';
 import Card from "./component/Card";
 
-ReactDOM.render(<Card/>, document.getElementById('root'));
+ReactDOM.render(<Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
