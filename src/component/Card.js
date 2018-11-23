c7e9d1ba299e0a446a9d812162edcf3c0e032cfd
christine-aqui <aqui.christine@gmail.com>
2018-11-23 12:54:03 -0500

    first commit

---
+++ src/component/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = () => {
+  return (
+    <div>
+      <img src='https://robohash.org/test' alt='robots'/>
+      <div>
+        <h2>Name</h2>
+        <p>email.name@mgial.com</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
