1b1095a2af5389d6061c72419adc3bbe0f308b8f
Christine Aqui <2739361+christine-aqui@users.noreply.github.com>
2018-12-13 15:03:06 -0500

    refactoring

--- src/containers/App.js
+++ src/containers/App.js
@@ -27,24 +27,23 @@ class App extends Component {
   }
 
   render() {
-    const filteredRobots = this.state.robots.filter(robot => {
-      return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     })
-    if(this.state.robots.length === 0 ){
-      return <h1>Loading</h1>
-    } else {
-      return (
+     return (!robots.length) ? <h1>Loading</h1> : 
+       (
         <div className='tc'>
           <h1 className='f1' >Robo Friends List</h1>
           <SearchBox  
-          searchfield={this.state.searchfield} 
+          searchfield={searchfield} 
           onSearchChange={this.onSearchChange}/>
           <Scroll >
             <CardList robots={filteredRobots}/>
           </Scroll>
         </div>
       )
-    }
+    
 
   }
 }
